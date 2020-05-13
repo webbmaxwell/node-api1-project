4025c23d4111484c338265c4a1884007460480f6
webbmaxwell <webbwrites@gmail.com>
2020-05-13 09:06:44 -0600

    Started building endpoints.

--- index.js
+++ index.js
@@ -2,14 +2,47 @@ const express = require('express');
 
 const server = express();
 
-let hubs = [];
-let lessons = [];
+//let hubs = [];
+//let lessons = [];
+
+server.listen(8000, () => console.log('API running on port 8000'));
 
 server.get('/', (req, res) => {
-  res.json({message: "Hello" })
+  res.send("Hello")
 });
 
-server.get('/users')
+server.get('/users', (req, res) => {
+  const users = [
+    {
+      id: 1,
+      name: 'Mario'
+    }
+    {
+      id: 2,
+      name: 'Donkey Kong'
+    }
+    {
+      id: 3,
+      name: 'Link'
+    }
+  ]
+})
+
+server.post('/users', (req, res) => {
+  res.
+})
+
+server.get('/users/:id', (req, res) => {
+  res.
+})
+
+server.delete('/users/:id', (req, res) => {
+  res.
+})
+
+server.patch('/users/:id', (req, res) => {
+  res.
+})
 
 //-------------------------------------
 const PORT = 5000;
