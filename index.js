f88ca8d6d0b51fa552615507ef404b1c451710cb
webbmaxwell <webbwrites@gmail.com>
2020-05-13 20:59:42 -0600

    Added more to endpoints and confirmed that responses send correctly

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const server = express();
 //let hubs = [];
 //let lessons = [];
 
-server.listen(8000, () => console.log('API running on port 8000'));
-
 server.get('/', (req, res) => {
-  res.send("Hello")
+  res.send("Hello there! Here is a web server!")
 });
 
 server.get('/users', (req, res) => {
@@ -16,32 +14,34 @@ server.get('/users', (req, res) => {
     {
       id: 1,
       name: 'Mario'
-    }
+    },
     {
       id: 2,
       name: 'Donkey Kong'
-    }
+    },
     {
       id: 3,
       name: 'Link'
-    }
-  ]
+    },
+  ];
+
+  res.status(200).send(users);
 })
 
 server.post('/users', (req, res) => {
-  res.
+  res.status(201).json({ url: '/users', operation: 'POST' });
 })
 
 server.get('/users/:id', (req, res) => {
-  res.
+  res.status(200)
 })
 
 server.delete('/users/:id', (req, res) => {
-  res.
+  res.status(204);
 })
 
-server.patch('/users/:id', (req, res) => {
-  res.
+server.put('/users/:id', (req, res) => {
+  res.status(200).json({ url: '/users/:id', operation: 'PUT'})
 })
 
 //-------------------------------------
